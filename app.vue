f00c9bdf92f8cfc1f71a94ce7ef1c852cae5dbf5
Game Thanapon <134900556+TopcloudThanapon@users.noreply.github.com>
2024-09-01 16:16:25 +0700

    remove : all services

--- app.vue
+++ app.vue
@@ -3,14 +3,7 @@
     <NuxtLayout>
       <NuxtPage />
     </NuxtLayout>
-    <ToastService />
-    <ConfirmModalService />
-    <LoadingModalService />
   </div>
 </template>
 
-<script lang="ts" setup>
-import ConfirmModalService from "./components/parts/services/ConfirmModalService.vue";
-import LoadingModalService from "./components/parts/services/LoadingModalService.vue";
-import ToastService from "./components/parts/services/ToastService.vue";
-</script>
+<script lang="ts" setup></script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,7 @@
 <template>
-  <div class="flex flex-col">
-    <AppBasicButton label="confirm" @click.prevent="onClick" />
-  </div>
+  <div>welcome</div>
 </template>
 
-<script lang="ts" setup>
-const { $dialog } = useNuxtApp();
-
-function onClick() {
-  $dialog.proceed(async () => {
-    alert("start");
-    await new Promise((resolve) => setTimeout(resolve, 1000));
-    throw new Error("some error");
-  });
-}
-</script>
+<script lang="ts" setup></script>
 
 <style></style>
