df15845479c851ca7266c8f769f8d7caf0f892c4
Game Thanapon <134900556+TopcloudThanapon@users.noreply.github.com>
2024-09-01 16:53:42 +0700

    xx

--- app.vue
+++ app.vue
@@ -3,7 +3,19 @@
     <NuxtLayout>
       <NuxtPage />
     </NuxtLayout>
+<<<<<<< HEAD
   </div>
 </template>
 
 <script lang="ts" setup></script>
+=======
+    <ToastService />
+    <ConfirmModalService />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import ConfirmModalService from "./components/parts/services/ConfirmModalService.vue";
+import ToastService from "./components/parts/services/ToastService.vue";
+</script>
+>>>>>>> parent of bc28b95 (add : loading and general plugin)
--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,5 @@
 <template>
+<<<<<<< HEAD
   <div>
     <Button label="hello" @click.prevent="toggleDarkMode" />
   </div>
@@ -9,5 +10,11 @@ function toggleDarkMode() {
   document.documentElement.classList.toggle("dark");
 }
 </script>
+=======
+  <div class="flex flex-col"></div>
+</template>
+
+<script lang="ts" setup></script>
+>>>>>>> parent of bc28b95 (add : loading and general plugin)
 
 <style></style>
